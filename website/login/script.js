fbee95cb3cf8c362f401a71162d0f1912247bdb9
rohitpalsaniya987 <111239742+rohitpalsaniya987@users.noreply.github.com>
2023-12-29 09:20:37 +0530

    Add files via upload

---
+++ website/login/script.js
@@ -0,0 +1,12 @@
+function redirectToLoginPage() {
+  // Redirect to the login page (replace the URL with your actual login page)
+  window.location.href = "../signup/signup.html";
+}
+
+// After 0.5 seconds, remove the loading element
+setTimeout(() => {
+  const loadingElement = document.getElementById('loading');
+  if (loadingElement) {
+    loadingElement.style.display = 'none';
+  }
+}, 500);
